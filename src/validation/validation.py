7934714ebc8188a47d25c78fa35faa0fcabc21bf
ali.rahmati <ali.rahmati@outlook.com>
2021-08-30 15:49:51 +0430

    [add] add validation module

---
+++ src/validation/validation.py
@@ -0,0 +1,11 @@
+def is_in_range(item, start_range, end_range):
+    """
+
+    :param item:
+    :param start_range:
+    :param end_range:
+    :return:
+    """
+    if start_range <= item <= end_range:
+        return True
+    return False
