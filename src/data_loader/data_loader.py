f1ecd2e6f13d59c8a00b9eae0fa05e78605f6cc4
ali.rahmati <ali.rahmati@outlook.com>
2021-09-05 12:34:49 +0430

    add shuffle to read data frame

--- src/data_loader/data_loader.py
+++ src/data_loader/data_loader.py
@@ -12,6 +12,6 @@ def read_csv(path: str, columns: list = None, names: list = None) -> pd.DataFram
     :param names:
     :return:
     """
-    dataframe = pd.read_csv(path, usecols=columns) if columns else pd.read_csv(path)
+    dataframe = pd.read_csv(path, usecols=columns).sample(frac=1) if columns else pd.read_csv(path).sample(frac=1)
     return dataframe.rename(columns={c: n for c, n in zip(columns, names)}) if names else dataframe
 
